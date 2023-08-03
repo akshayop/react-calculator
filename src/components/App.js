05225209dfcb7ec954904d440b733857664ebb11
@k$hay <akshaynaik5787@gmail.com>
2023-08-03 19:24:16 +0530

    added buttons in button component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
+
 import Buttons from "./Buttons";
 import Display from "./Display";
 
 function App() {
+
   return (
     <div className="App">
       <Display/>
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,8 +1,48 @@
-const Buttons = () => {
-    return(
-        <>
-        </>
-    )
-}
+const Buttons = ({clear, input, plusMinus, totalAns}) => {
 
+
+    return(
+        <div> 
+            <table>
+
+                <tbody>
+                    <tr>
+                        
+                        <th onClick={clear}>C</th>
+                        <th onClick={plusMinus}> +/-</th>
+                        <th onClick={input}> % </th>
+                        <th onClick={input}> / </th>
+                    </tr>
+
+                    <tr>
+                        <th onClick={input}> 7 </th>
+                        <th onClick={input}> 8 </th>
+                        <th onClick={input}> 9 </th>
+                        <th onClick={input}> * </th>
+                    </tr>
+
+                    <tr>
+                        <th onClick={input}> 4 </th>
+                        <th onClick={input}> 5 </th>
+                        <th onClick={input}> 6 </th>
+                        <th onClick={input}> - </th>
+                    </tr>
+
+                    <tr>
+                        <th onClick={input}> 1 </th>
+                        <th onClick={input}> 2 </th>
+                        <th onClick={input}> 3 </th>
+                        <th rowSpan={2} onClick={input}> + </th>
+                    </tr>
+
+                    <tr>
+                        <th onClick={input}> . </th>
+                        <th onClick={input}> 0 </th>
+                        <th onClick={totalAns}> = </th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+} 
 export default Buttons;
